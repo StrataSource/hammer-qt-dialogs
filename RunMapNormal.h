f4c080b6e6406db13713413116fa82fc42696213
ozxybox <kieferstashu@gmail.com>
2021-09-15 16:28:21 -0500

    feat: run map normal dialog

---
+++ RunMapNormal.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <QDialog>
+#include <QButtonGroup>
+#include <QCheckBox>
+#include <QLineEdit>
+
+
+namespace ui
+{
+
+class CRunMapNormal : public QDialog
+{
+	Q_OBJECT
+public:
+
+	// The button groups use this enum for ids
+	enum CompilerMode
+	{
+		NO,
+		NORMAL,
+		FAST,
+		ONLY_ENTS
+	};
+
+	QButtonGroup* m_pBspRadioGroup;
+	QButtonGroup* m_pVisRadioGroup;
+	QButtonGroup* m_pRadRadioGroup;
+	QCheckBox*    m_pRadHDRCheck;
+
+	QCheckBox*    m_pWaitForKeyCheck;
+	QCheckBox*    m_pDontRunGameCheck;
+
+	QLineEdit* m_pGameParameters;
+
+	CRunMapNormal(QWidget* pParent);
+
+private slots:
+	void onOkPressed();
+	void onExpertPressed();
+	void onCancelPressed();
+
+};
+
+}
