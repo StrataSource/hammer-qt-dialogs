53a5c5c41ee416d27f323e33cd95421cc236cd62
Smae De <mitrblue@gmail.com>
2021-09-19 18:01:27 +1000

    feat: Entity report dialog

---
+++ dialogs/EntityReport.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <QDialog>
+#include <QButtonGroup>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QComboBox.h>
+
+namespace ui
+{
+	class CEntityReport : public QDialog
+	{
+		Q_OBJECT
+	public:
+		CEntityReport( QWidget *pParent );
+
+		enum FilterMode
+		{
+			EVERYTHING,
+			BRUSH_ENTS,
+			POINT_ENTS,
+		};
+
+		QButtonGroup *m_pFilterModeRadioGroup;
+
+		QCheckBox *m_pFilterIncludeHiddenCheck;
+
+		QCheckBox *m_pFilterByKVCheck;
+		QCheckBox *m_pFilterKVExactCheck;
+
+		QLineEdit *m_pFilterKey;
+		QLineEdit *m_pFilterVal;
+
+		QCheckBox *m_pFilterByClassCheck;
+
+	private slots:
+		void onGoToPressed();
+		void onDeletePressed();
+		void onPropertiesPressed();
+
+		void onClosePressed();
+	};
+
+} // namespace ui
