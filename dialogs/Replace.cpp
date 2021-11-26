e94fc941db1cd9dbb6585e3ba80facc9ab8d2c1e
Erin <28122025+AWildErin@users.noreply.github.com>
2021-11-26 19:24:32 +0000

    feat: Replace dialog

---
+++ dialogs/Replace.cpp
@@ -0,0 +1,112 @@
+#include "dialogs/Replace.h"
+
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGroupBox>
+#include <QGridLayout>
+#include <QLabel>
+#include <QPushButton>
+
+using namespace ui;
+
+CReplace::CReplace( QWidget *pParent ) :
+	QDialog( pParent )
+{
+	this->setWindowTitle( tr( "Replace" ) );
+
+	auto pDialogLayout = new QGridLayout( this );
+
+	auto pFindLabel = new QLabel( tr( "Find what:" ), this );
+	m_pFindBox = new QLineEdit( this );
+	auto pFindBoxLayout = new QHBoxLayout( this );
+	pFindBoxLayout->setSpacing( 20 );
+	pFindBoxLayout->addWidget( pFindLabel );
+	pFindBoxLayout->addWidget( m_pFindBox );
+
+	auto pReplaceLabel = new QLabel( tr( "Replace with:" ), this );
+	m_pReplaceBox = new QLineEdit( this );
+	auto pReplaceBoxLayout = new QHBoxLayout( this );
+	pReplaceBoxLayout->addWidget( pReplaceLabel );
+	pReplaceBoxLayout->addWidget( m_pReplaceBox );
+
+
+	// Push Buttons
+	auto pFindNextButton = new QPushButton( tr( "Find Next" ), this );
+	auto pReplaceButton = new QPushButton( tr( "Replace" ), this );
+	auto pReplaceAllButton = new QPushButton( tr( "Replace All" ), this );
+	auto pCancelButton = new QPushButton( tr( "Cancel" ), this );
+
+	// Checkboxes
+	m_pWholeWordsOnly = new QCheckBox( tr( "Whole words only" ), this );
+	m_pCaseSensitive = new QCheckBox( tr( "Case Sensitive" ), this );
+
+	// Find In box
+	auto pFindInGroup = new QGroupBox( tr( "Find in" ), this );
+	auto pFindInLayout = new QVBoxLayout( pFindInGroup );
+	m_pSelectionButton = new QRadioButton( tr( "Selection" ), this );
+	m_pSelectionButton->setChecked( true );
+	m_pEntireFileButton = new QRadioButton( tr( "Entire File" ), this );
+	m_pVisibleObjectsOnly = new QCheckBox( tr( "Visible objects only" ), this );
+
+	pFindInLayout->addWidget( m_pSelectionButton );
+	pFindInLayout->addWidget( m_pEntireFileButton );
+	pFindInLayout->addWidget( m_pVisibleObjectsOnly );
+
+
+	// Add all the widgets to the layout
+	int row = 0;
+
+	//0
+	//pDialogLayout->addWidget( pFindLabel, row, 0 );
+	//pDialogLayout->addWidget( m_pFindBox, row, 1, 1, 2 );
+	pDialogLayout->addLayout( pFindBoxLayout, row, 0, 1, 3 );
+	pDialogLayout->addWidget( pFindNextButton, row, 3 );
+	row++;
+
+	//1
+	//pDialogLayout->addWidget( pReplaceLabel, row, 0 );
+	//pDialogLayout->addWidget( m_pReplaceBox, row, 1, 1, 2 );
+	pDialogLayout->addLayout( pReplaceBoxLayout, row, 0, 1, 3 );
+	pDialogLayout->addWidget( pReplaceButton, row, 3 );
+	row++;
+
+	//2
+	pDialogLayout->addWidget( m_pWholeWordsOnly, row, 0 );
+	pDialogLayout->addWidget( pFindInGroup, row, 1, 3, 2 );
+	pDialogLayout->addWidget( pReplaceAllButton, row, 3 );
+	row++;
+
+	//3
+	pDialogLayout->addWidget( m_pCaseSensitive, row, 0 );
+	pDialogLayout->addWidget( pCancelButton, row, 3 );
+
+	// Setup actions
+	connect( pFindNextButton, &QPushButton::released, this, &CReplace::onFindNextPressed );
+	connect( pReplaceButton, &QPushButton::released, this, &CReplace::onReplacePressed );
+	connect( pReplaceAllButton, &QPushButton::released, this, &CReplace::onReplaceAllPressed );
+	connect( pCancelButton, &QPushButton::released, this, &CReplace::onCancelPressed );
+
+
+	this->setLayout( pDialogLayout );
+	this->setFixedSize( this->sizeHint() );
+}
+
+void CReplace::onFindNextPressed()
+{
+	// QT_TODO: hammer integration
+}
+
+void CReplace::onReplacePressed()
+{
+	// QT_TODO: hammer integration
+}
+
+void CReplace::onReplaceAllPressed()
+{
+	// QT_TODO: hammer integration
+}
+
+void CReplace::onCancelPressed()
+{
+	this->close();
+}
